1f1c715b79578f9ca3a3b3f50d8cd60041450967
eosfor <9363027+eosfor@users.noreply.github.com>
2025-04-22 06:49:48 +0000

    config update

--- observablehq.config.js
+++ observablehq.config.js
@@ -1,22 +1,12 @@
 // See https://observablehq.com/framework/config for documentation.
 import MarkdownItContainer from "markdown-it-container";
+import { readdirSync } from "fs";
+import { join } from "path";
+import { generatePages } from "./helpers/generatePages.js";
 
 export default {
   // The project’s title; used in the sidebar and webpage titles.
   title: "Modern IT",
-
-  // The pages and sections in the sidebar. If you don’t specify this option,
-  // all pages will be listed in alphabetical order. Listing pages explicitly
-  // lets you organize them into sections and have unlisted pages.
-  // pages: [
-  //   {
-  //     name: "Examples",
-  //     pages: [
-  //       {name: "Dashboard", path: "/example-dashboard"},
-  //       {name: "Report", path: "/example-report"}
-  //     ]
-  //   }
-  // ],
 
   // Content to add to the head of the page, e.g. for a favicon:
   head: '<link rel="icon" href="observable.png" type="image/png" sizes="32x32">',
@@ -35,15 +25,18 @@ export default {
   },
 
   // Some additional configuration options and their defaults:
-  theme: "default", // try "light", "dark", "slate", etc.
+  theme: ["default"], // try "light", "dark", "slate", etc.
   // header: "", // what to show in the header (HTML)
   // footer: "Built with Observable.", // what to show in the footer (HTML)
   // sidebar: true, // whether to show the sidebar
   toc: true, // whether to show the table of contents
   pager: true, // whether to show previous & next links in the footer
   // output: "dist", // path to the output root for build
-  // search: true, // activate search
+  search: true, // activate search
   // linkify: true, // convert URLs in Markdown to links
-  typographer: true // smart quotes and other typographic improvements
+  typographer: true, // smart quotes and other typographic improvements
   // cleanUrls: true, // drop .html from URLs
+
+
+  pages: generatePages("src")
 };
